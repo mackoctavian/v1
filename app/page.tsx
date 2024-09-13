b5bc354c36f5584e51b4914150a0155085325464
Mack Octavian <maestrooctavian@gmail.com>
2024-09-14 01:10:06 +0300

    Cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -6,23 +6,23 @@ import { AuroraBackground } from "@/components/ui/aurora-background";
 import { TextHoverEffect } from "@/components/ui/text-hover-effect";
 import Logo from "./mack-white.png";
 import Image from "next/image";
-import { useScroll, useTransform } from "framer-motion";
-import { LinearGradient } from "react-text-gradients";
+// import { useScroll, useTransform } from "framer-motion";
 
-import { GoogleGeminiEffect } from "@/components/ui/google-gemini-effect";
+// import { GoogleGeminiEffect } from "@/components/ui/google-gemini-effect";
 
 export default function Home() {
   return (
     <main>
-      {/* <nav className="w-full bg-transparent ">
-        <ul className="flex items-center justify-between ">
-          <li>
-            <Image src={Logo} height={40} width={40} alt="logo" />
-          </li>
-          <li className="hidden">Download</li>
-        </ul>
-      </nav> */}
       <AuroraBackground>
+        <nav className="w-full bg-transparent top-0">
+          <ul className="flex items-center justify-between ">
+            <li>
+              <Image src={Logo} height={40} width={40} alt="logo" />
+            </li>
+            <li className="hidden">Download</li>
+          </ul>
+        </nav>
+
         <motion.div
           initial={{ opacity: 0.0, y: 40 }}
           whileInView={{ opacity: 1, y: 0 }}
@@ -44,33 +44,33 @@ export default function Home() {
   );
 }
 
-function GoogleGeminiEffectDemo() {
-  const ref = React.useRef(null);
-  const { scrollYProgress } = useScroll({
-    target: ref,
-    offset: ["start start", "end start"],
-  });
+// function GoogleGeminiEffectDemo() {
+//   const ref = React.useRef(null);
+//   const { scrollYProgress } = useScroll({
+//     target: ref,
+//     offset: ["start start", "end start"],
+//   });
 
-  const pathLengthFirst = useTransform(scrollYProgress, [0, 0.8], [0.2, 1.2]);
-  const pathLengthSecond = useTransform(scrollYProgress, [0, 0.8], [0.15, 1.2]);
-  const pathLengthThird = useTransform(scrollYProgress, [0, 0.8], [0.1, 1.2]);
-  const pathLengthFourth = useTransform(scrollYProgress, [0, 0.8], [0.05, 1.2]);
-  const pathLengthFifth = useTransform(scrollYProgress, [0, 0.8], [0, 1.2]);
+//   const pathLengthFirst = useTransform(scrollYProgress, [0, 0.8], [0.2, 1.2]);
+//   const pathLengthSecond = useTransform(scrollYProgress, [0, 0.8], [0.15, 1.2]);
+//   const pathLengthThird = useTransform(scrollYProgress, [0, 0.8], [0.1, 1.2]);
+//   const pathLengthFourth = useTransform(scrollYProgress, [0, 0.8], [0.05, 1.2]);
+//   const pathLengthFifth = useTransform(scrollYProgress, [0, 0.8], [0, 1.2]);
 
-  return (
-    <div
-      className="h-[400vh] bg-black w-full dark:border dark:border-white/[0.1] rounded-md relative pt-40 overflow-clip"
-      ref={ref}
-    >
-      <GoogleGeminiEffect
-        pathLengths={[
-          pathLengthFirst,
-          pathLengthSecond,
-          pathLengthThird,
-          pathLengthFourth,
-          pathLengthFifth,
-        ]}
-      />
-    </div>
-  );
-}
+//   return (
+//     <div
+//       className="h-[400vh] bg-black w-full dark:border dark:border-white/[0.1] rounded-md relative pt-40 overflow-clip"
+//       ref={ref}
+//     >
+//       <GoogleGeminiEffect
+//         pathLengths={[
+//           pathLengthFirst,
+//           pathLengthSecond,
+//           pathLengthThird,
+//           pathLengthFourth,
+//           pathLengthFifth,
+//         ]}
+//       />
+//     </div>
+//   );
+// }
